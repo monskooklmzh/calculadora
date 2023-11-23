fb0c54784dad41ca73a481028c348ab1def3147a
luz_marinazh <110910640+monskooklmzh@users.noreply.github.com>
2023-11-23 09:04:37 -0300

    Add files via upload

---
+++ exe.js
@@ -0,0 +1,29 @@
+fuction contar(){
+	var numinicio=
+	document.getElementByld('numinicio')
+	var numFim= document.getElementByld('numFim')
+	var passo1=document.getElementByld('passo')
+	var numln= Number(numinicio.value)
+	var numf=Number('numFim.value')
+	var passo= Number(passo1.value)
+	var res= document.getElementByld('res')
+	if(numinicio.value.length==0)
+	res.inner Text=" impossivel contar!")
+    else if(passo==0)
+    (aler(passo invalido, considerando passo=1)
+    var passo=1 
+    while(numin<=numF)
+    res.innerText+='$'(numin)
+    numin=numin+ passo
+    {
+
+    } else{
+    	for (;numin<=numF; numin= numin+ passo)}
+    		res.innerText+='$(numin)'
+    }
+}
+/while(numin<=numF){
+	res.innerText+=	'$(numin)'
+	numin= numin+passo*/
+}
+
